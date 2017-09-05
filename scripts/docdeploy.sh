e2e6b6c009d3ac2c0725da74849b9ac457e0f8f4
Miles Petrov <miles@petrovs.ca>
2017-09-05 09:24:57 -0400

    auto deploy api docs

--- scripts/docdeploy.sh
+++ scripts/docdeploy.sh
@@ -23,3 +23,23 @@ if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/fgpv-vpgf" ] && [ -n "$TRAVIS_TAG" ]; the
     cd ..
     rm -rf ghdocs
 fi
+
+if [ "$TRAVIS_BRANCH" == "api" ]; then
+    npm run apidoc
+    # this section assumes the id_rsa key has already been decrypted
+    # devdeploy.sh should run before this script
+    echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+    eval `ssh-agent -s`
+    ssh-add ~/.ssh/id_rsa
+    git clone --depth=1 git@github.com:fgpv-vpgf/fgpv-vpgf.github.io.git ghdocs
+    mkdir -p ghdocs/fgpv-vpgf/$TRAVIS_BRANCH
+    rsync -av --delete "apidocs/" "ghdocs/fgpv-vpgf/$TRAVIS_BRANCH"
+    cd ghdocs
+    git add fgpv-vpgf/$TRAVIS_BRANCH
+    git config user.email "glitch.chatbot@gmail.com"
+    git config user.name "Glitch Bot"
+    git commit -m "Docs for fgpv@$TRAVIS_BRANCH"
+    git push
+    cd ..
+    rm -rf ghdocs
+fi
